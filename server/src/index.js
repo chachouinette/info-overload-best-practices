4222438520db5d72083df98bf09d1ef57077cc44
Chachouinette <charlotte.gaia@gmail.com>
2025-09-14 16:08:14 +0200

    Prepare the build and deployment of the app

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { bestPractices } = require('../bestPractices');
@@ -12,6 +13,13 @@ app.get('/api/best-practices', (req, res) => {
   res.json(bestPractices);
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+});
+
+// Start the server
 app.listen(PORT, () => {
   console.log(`API listening on http://localhost:${PORT}`);
 });
